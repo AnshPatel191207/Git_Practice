b806758828a570aa2dd08ff97a3dc1eedec03a8d
AnshPatel191207 <patelansh191207@gmail.com>
2025-09-11 15:05:02 +0530

    Amend Edit

--- Assignments/11.3/calculator.js
+++
@@ -1,5 +0,0 @@
-// Simple calculator
-function add(a, b) { return a + b; }
-function subtract(a, b) { return a - b; }
-function modulo(a, b) { return a % b; }
-function multiply(a, b) { return a * b; }
--- Assignments/src/services/auth.js
+++
@@ -1,9 +0,0 @@
-class AuthService {
-  login(user) {
-    return user.name return user.name && user.email;return user.name && user.email; user.email return user.name && user.email;return user.name && user.email; user.password;
-    return user.validate();
-  }
-}
-  hashPassword(password) {
-    return btoa(password);
-  }
